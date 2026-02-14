31af9e541a7ebe9508aa2d8a652f074f2c3055c3
Emmanuel Guefif <eguefif@fastmail.com>
2026-02-14 23:42:54 +0100

    Refactor tokenenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,5 +1,9 @@
 #include "shell.h"
 
+Bool is_whitespace(char);
+int skip_white_char(char *);
+char *get_string(char *);
+
 TokenType get_token_type(char *value) {
   if (strncmp(value, "\"", 1) == 0) {
     return DQUOTE;
@@ -20,20 +24,52 @@ TokenType get_token_type(char *value) {
 }
 
 Token *tokenize(char *input) {
-  Token *tokens = (Token *)malloc(sizeof(Token) * 100);
-  int count = 0;
+  Token *tokens = (Token *)malloc(sizeof(Token) * 200);
+  int cursor = 0;
+  int token_count = 0;
+  char *str;
 
-  char *value = strtok(input, " ");
-
-  while (value != NULL) {
-    TokenType type = get_token_type(value);
-    Token token = {type, value};
-    tokens[count] = token;
-    count++;
-    value = strtok(NULL, " ");
+  cursor = skip_white_char(input);
+  while (input[cursor] != '\0') {
+    switch (input[cursor]) {
+    case '\'':
+      tokens[token_count] = (Token){QUOTE, &input[cursor]};
+      token_count++;
+      cursor++;
+      break;
+    default:
+      str = get_string(&input[cursor]);
+      // We add one because str is a pointer to a substring of input
+      // We just replace the first space after the string by \0
+      // The cursor needs to be moved after this \0
+      cursor += strlen(str) + 1;
+      cursor += skip_white_char(input);
+      tokens[token_count] = (Token){STRING, str};
+      token_count++;
+      break;
+    }
   }
   Token token = {EOP, ""};
-  tokens[count] = token;
+  tokens[token_count] = token;
 
   return tokens;
+}
+
+int skip_white_char(char *input) {
+  int cursor = 0;
+  while (is_whitespace(input[cursor]) && !(input[cursor] == '\0')) {
+    cursor++;
+  }
+  return cursor;
+}
+
+Bool is_whitespace(char c) { return c == ' ' || c == '\t' || c == '\n'; }
+
+char *get_string(char *input) {
+  int cursor = 0;
+  while (!is_whitespace(input[cursor])) {
+    cursor++;
+  }
+  input[cursor] = '\0';
+  return input;
 }
